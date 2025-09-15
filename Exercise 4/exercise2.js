78f9cf5b0bc22bc125228290f7c488fa85b8fb9c
Prisca <priscamasereli@gmail.com>
2025-09-15 14:57:31 +0200

    feat: Add exercise 4 question2

---
+++ Exercise 4/exercise2.js
@@ -0,0 +1,32 @@
+// convert integer → Roman numeral
+
+function romanToInt(roman) {
+  const values = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  };
+
+  let total = 0;
+
+  for (let i = 0; i < roman.length; i++) {
+    const current = values[roman[i]];
+    const next = values[roman[i + 1]];
+
+    if (next && current < next) {
+      total -= current;
+    } else {
+      total += current;
+    }
+  }
+
+  return total;
+}
+
+console.log(romanToInt("XC"));
+console.log(romanToInt("XL"));
+console.log(romanToInt("I"));
